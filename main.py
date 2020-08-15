1f526001abe9aab227d64acdcf2ebed2031e20f8
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-15 23:47:49 +0400

    Create an optimization

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+import numpy as np
 from copy import deepcopy
-from numba import jit
+from numba import njit
 from random import randint
 
 
@@ -16,27 +17,34 @@ pygame.init()
 game_screen = pygame.display.set_mode(RESOLUTION)
 clock = pygame.time.Clock()
 
-next_field = [[0 for i in range(W)] for j in range(H)]
-current_field = [[0 for i in range(W)] for j in range(H)]
-current_field = [[1 if i == W // 2 or j == H // 2 else 0 for i in range(W)] for j in range(H)]
+next_field = np.array([[0 for i in range(W)] for j in range(H)])
+current_field = np.array([[1 if not (i * j) % 22 else 0 for i in range(W)] for j in range(H)])
 
+@njit(fastmath=True)
+def check_cells(current_field, next_field):
+    res = []
+    for x in range(1, W - 1):
+        for y in range(1, H - 1):
+            count = 0
+            for j in range(y - 1, y + 2):
+                for i in range(x - 1, x + 2):
+                    if current_field[j][i] == 1:
+                        count += 1
 
-def check_cell(current_field, x, y):
-    count = 0
-    for j in range(y - 1, y + 2):
-        for i in range(x - 1, x + 2):
-            if current_field[j][i]:
-                count += 1
-
-    if current_field[y][x]:
-        count -= 1
-        if count == 2 or count == 3:
-            return 1
-        return 0
-    else:
-        if count == 3:
-            return 1
-        return 0
+            if current_field[y][x] == 1:
+                count -= 1
+                if count == 2 or count == 3:
+                    next_field[y][x] = 1
+                    res.append((x, y))
+                else:
+                    next_field[y][x] = 0
+            else:
+                if count == 3:
+                    next_field[y][x] = 1
+                    res.append((x, y))
+                else:
+                    next_field[y][x] = 0
+    return next_field, res
 
 
 while True:
@@ -46,14 +54,13 @@ while True:
         if event.type == pygame.QUIT:
             exit()
 
-    [pygame.draw.line(game_screen, pygame.Color('darkslategray'), (x, 0), (x, HEIGHT)) for x in range(0, WIDTH, TILE)]
-    [pygame.draw.line(game_screen, pygame.Color('darkslategray'), (0, y), (WIDTH, y)) for y in range(0, HEIGHT, TILE)]
+    [pygame.draw.line(game_screen, pygame.Color('black'), (x, 0), (x, HEIGHT)) for x in range(0, WIDTH, TILE)]
+    [pygame.draw.line(game_screen, pygame.Color('black'), (0, y), (WIDTH, y)) for y in range(0, HEIGHT, TILE)]
+    
     # draw life
-    for x in range(1, W - 1):
-        for y in range(1, H - 1):
-            if current_field[y][x]:
-                pygame.draw.rect(game_screen, pygame.Color('forestgreen'), (x * TILE + 2, y * TILE + 2, TILE - 2, TILE - 2))
-            next_field[y][x] = check_cell(current_field, x, y)
+    next_field, res = check_cells(current_field, next_field)
+    [pygame.draw.rect(game_screen, pygame.Color("darkorange"),
+                      (x * TILE + 1, y * TILE + 1, TILE - 1, TILE - 1)) for x, y in res]
 
     current_field = deepcopy(next_field)
 
