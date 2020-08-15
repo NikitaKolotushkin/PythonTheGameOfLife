95330bd6721ac88e480cfcd59a400422df9a537c
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-15 23:20:06 +0400

    Add a main window

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello world!")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pygame
+from copy import deepcopy
+from numba import jit
+from random import randint
+
+
+RESOLUTION = WIDTH, HEIGHT = 1600, 900
+TILE = 5
+W, H = WIDTH // TILE, HEIGHT // TILE
+FPS = -1
+
+pygame.init()
+game_screen = pygame.display.set_mode(RESOLUTION)
+clock = pygame.time.Clock()
+
+while True:
+
+    game_screen.fill(pygame.Color('black'))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    # print(clock.get_fps())
+    pygame.display.flip()
+    clock.tick(FPS)
